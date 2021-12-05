05b78aacbb977d8fa4cb0f3a09ecddf9ff062536
Christophe Prudent <christophe.prudent@laposte.fr>
2021-12-05 18:15:20 +0100

    continue on linked list, always learning!

--- linked list/e009_linked_list_doubly.py
+++ linked list/e009_linked_list_doubly.py
@@ -59,6 +59,37 @@ class doubly_linked_list(object):
         self.head = self.tail
         self.tail = temp
 
+    def insert_first(self, data):
+        new_item = Node(data, self.head, None)
+        self.head.prev = new_item
+        self.head = new_item
+        self.count += 1
+
+    def find(self, data):
+        for val in self.iter():
+            if val == data:
+                return True
+        return False
+
+    def delete_item(self, data):
+        # Delete an item from the list
+        current = self.head
+        while current:
+            if current.data == data:
+                if current == self.head:
+                    (self.head.next).prev = None
+                    self.head = self.head.next
+                elif current == self.tail:
+                    (self.tail.prev).next = None
+                    self.tail = self.tail.prev
+                else:
+                    (current.prev).next = current.next
+                    (current.next).prev = current.prev
+                del current
+                self.count -= 1
+                return
+            current = current.next
+
 items = doubly_linked_list()
 items.append_item('PHP')
 items.append_item('Python')
@@ -74,4 +105,26 @@ items.print_forward()
 print("print reversed items:")
 items.reverse()
 items.print_forward()
+print("insert new item at top, print forwards:")
+items.insert_first('Pascal')
+items.print_forward()
+print("print backwards:")
+items.print_backward()
 
+# recherche par valeur
+while True:
+    data = input("Entrez la valeur à chercher (vide pour quitter): ")
+    if not data:
+        break
+
+    _find = items.find(data)
+    print("dans la liste: ", _find)
+
+# delete items
+print("delete 2 items:")
+items.delete_item('C#')
+items.delete_item('PHP')
+print("print forwards:")
+items.print_forward()
+print("print backwards:")
+items.print_backward()
